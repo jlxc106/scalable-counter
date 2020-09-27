78371f52e252e88d1ca2fcc07e84dd4f2a40ee71
jlxc106 <jaylim106@gmail.com>
2020-09-27 01:59:57 -0700

    flaskr setup

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 
 def create_app(test_config=None):
     # create and configure the app
+    print(f"my name is {__name__}")
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
@@ -28,5 +29,16 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
 
     return app
