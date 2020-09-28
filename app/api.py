3c80e9594fb68acf7d97d983603a977215f0731e
jlxc106 <jaylim106@gmail.com>
2020-09-27 22:18:37 -0700

    timer

--- app/api.py
+++ app/api.py
@@ -1,5 +1,5 @@
 import functools
-
+from time import time
 from flask import (
     Blueprint, jsonify
     # , flash, g, redirect, render_template, request, session, url_for
@@ -13,13 +13,14 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 
 @bp.route('/item', methods=('GET', ))
 def getItem():
+    start = time()
     # return "some string"
     key = "count1"
     try:
         c = redis_client.get(key).decode('utf-8')
     except AttributeError:
         c = None
-    print("count obj:", c)
+    # print("count obj:", c)
     if c == None:
         redis_client.set(key, 1)
         c = 1
@@ -27,4 +28,6 @@ def getItem():
         c = int(c)
         redis_client.set(key, c+1)
     d = {"count": c}
+    end = time()
+    print(f"{end - start} seconds to run /api/item")
     return jsonify(d)
