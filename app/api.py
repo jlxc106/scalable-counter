6eb1a7d57c0fd0609eda9344962aa8bdd4829d1f
Jay Lim <jaylim106@gmail.com>
2020-09-28 11:09:02 -0700

    update

--- app/api.py
+++ app/api.py
@@ -26,5 +26,25 @@ def getItem():
     else:
         c = int(c)+1
         redis_client.set(key, c)
-    d = {"count": c}
+    
+    db_c = 10
+    #hincrby - hash increment by amount
+#     #returns the updated value
+
+#     X HINCRBY s12345:unique_visitors:daily 20140920 1    // returns the current hit count
+#   SETNX   s12345:unique_visitors:sync 1              // succeeds because no sync has happened
+#   SETEX   s12345:unique_visitors:sync 60 1           // makes the sync lock key expire
+#   RENAME  s12345:unique_visitors:daily s12345:unique_visitors:daily_<RANDOM>  // isolates data for this sync
+#   HSCAN   s12345:unique_visitors:daily_<RANDOM> ...  // repeat until we have all the data
+#   * add results of hscan to MySQL *
+#   DEL     s12345:unique_visitors:daily_<RANDOM>
+    """
+    upd_val = redis_client.hincrby("name", "key",  amount=1)
+    if upd_val >49:
+        print("trigger db sync")
+    redis_client.setnx()
+    redix_client.setex()
+    """
+
+    d = {"count": c + db_c}
     return jsonify(d)
